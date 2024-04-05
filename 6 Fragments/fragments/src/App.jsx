ab989a414e447b82ef9a99219599a8fe57caf784
HirenGajjar <gajjarhiren111@gmail.com>
2024-04-05 11:46:46 -0700

    Add example for ternary operation

--- 6 Fragments/fragments/src/App.jsx
+++ 6 Fragments/fragments/src/App.jsx
@@ -3,11 +3,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-let food = ['a','b','c','d','e'];
+// let food = ['a','b','c','d','e'];
+let food =[];
 
+let msg = food.length == 0 ? <h2>Find something</h2> :null;
+if(food.length == 0){
+  return <h3>Find something</h3>;
+}
   return (
     <>
      <h1>Healthy food</h1>
+     {
+      msg
+     }
       <ul className="list-group">
             { 
                   food.map(item => <li key={item} className="list-group-item">{item}</li>)
