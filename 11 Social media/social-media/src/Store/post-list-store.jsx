14ba9806e9e51bf3046d5bc8b76b39cd90d77254
HirenGajjar <gajjarhiren111@gmail.com>
2024-04-10 12:24:17 -0700

    Add post list store

---
+++ 11 Social media/social-media/src/Store/post-list-store.jsx
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+
+
+
+const PostList = createContext({});
+
+
+
+
+const PostListProvider =({children})=>{
+    return <PostList.Provider>
+
+{
+    children
+}
+
+    </PostList.Provider>
+}
+export default PostListProvider
