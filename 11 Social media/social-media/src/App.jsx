1ac8c89a9b99ebf600293d7875afdcd0901dc803
HirenGajjar <gajjarhiren111@gmail.com>
2024-04-10 00:55:48 -0700

    Add sidebar component

--- 11 Social media/social-media/src/App.jsx
+++ 11 Social media/social-media/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <Sidebar/>
      <Header/>
      <Footer/>
   </>
